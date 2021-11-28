c6ed1a54ed063f459bddb957bd8f439391d9c08a
Myles Gary <mgary5@lsu.edu>
2021-11-28 17:48:00 -0600

    added Home screen setup to Screens.js

---
+++ assets/Screens.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    ImageBackground,
+    TouchableOpacity,
+    SafeAreaView,
+    ImageComponent,
+  } from "react-native";
+
+  export const Home = () => (
+      <View>
+          
+      </View>
+  )
